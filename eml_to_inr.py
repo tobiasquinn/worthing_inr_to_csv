b3a44b300155600a16a93221e21e57ed72cb880b
Tobias Quinn <tobias@tobiasquinn.com>
2019-11-05 22:09:58 +0000

    decode pdf and output as INR and date

--- eml_to_inr.py
+++ eml_to_inr.py
@@ -2,10 +2,14 @@
 import eml_parser
 import glob
 import base64
+import pdftotext
+import io, re
+import datetime
 
 filenames = glob.glob("eml/*.eml")
 
 # parse through each of the eml files, we are only expecting one attachment which is the pdf of results
+
 def eml_to_pdf(filename):
     with open(filename, 'rb') as fhdl:
         raw_email = fhdl.read()
@@ -13,6 +17,15 @@ def eml_to_pdf(filename):
         raw_attachment_data = parsed_eml['attachment'][0]['raw']
         return base64.b64decode(raw_attachment_data)
 
-pdf = [eml_to_pdf(filename) for filename in filenames]
-with open('test.pdf', 'wb') as p_out:
-    p_out.write(pdf[0])
+pdfs = [eml_to_pdf(filename) for filename in filenames]
+
+inr_regex = "Your INR result was:\W+(\d\.\d) on (\d\d?/\d\d?/\d\d\d\d)"
+data = []
+for pdf in pdfs:
+    pdf_text = pdftotext.PDF(io.BytesIO(pdf))
+    m = re.search(inr_regex, pdf_text[0])
+    day, month, year = m.group(2).split("/")
+    data.append([float(m.group(1)), datetime.date(int(year), int(month), int(day))])
+
+for d in data:
+    print(d)
