61542131b691a7b44446d301acab496b4ad02cfc
Tobias Quinn <tobias@tobiasquinn.com>
2019-11-06 00:09:46 +0000

    also output dose that was taken to get to INR reading

--- eml_to_inr.py
+++ eml_to_inr.py
@@ -26,16 +26,21 @@ def eml_to_pdf(filename):
 pdfs = [eml_to_pdf(filename) for filename in filenames]
 
 inr_pattern = r'Your INR result was:\W+(\d\.\d) on (\d\d?/\d\d?/\d\d\d\d)'
+# this picks out the previous periods dose from the current pdf
+dose_pattern = r'\W+Date\W+INR\W+Dose\W+Treatment summary\s\W+(\d\d/\d\d/\d\d\d\d)\W+\d\.\d\W+(\d\.\d\d)'
+dose_regex = re.compile(dose_pattern)
 inr_regex = re.compile(inr_pattern)
 data = []
 for pdf in pdfs:
     pdf_text = pdftotext.PDF(io.BytesIO(pdf))
     m = inr_regex.search(pdf_text[0])
     day, month, year = m.group(2).split("/")
+    n = dose_regex.search(pdf_text[0])
     data.append([float(m.group(1)),
-                 datetime.date(int(year), int(month), int(day))])
+                 datetime.date(int(year), int(month), int(day)),
+                 n.group(1), float(n.group(2))])
 
 data = sorted(data, key=itemgetter(1))
-print("Date,INR")
+print("Date,INR,Dose")
 for d in data:
-    print(d[1].isoformat() + "," + str(d[0]))
+    print(d[1].isoformat() + "," + str(d[0]) + "," + str(d[3]))
